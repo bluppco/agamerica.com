02f15e8ee3b5a62ac6fdc32907e5198b5494be44
Shiva pandey <shivapandey9898@gmail.com>
2024-07-16 16:31:17 +0530

    chore: added picture atom for jsx

---
+++ src/atoms/picture/internal/contain/picture-internal-contain.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+const PictureContain = ({ alternative_text, source }) => {
+    return (
+        <picture className="w-full h-full">
+            <source media="(max-width: 375px)" srcSet={source} />
+            <source media="(max-width: 640px)" srcSet={source} />
+            <img
+                alt={alternative_text}
+                className="w-full h-full object-contain"
+                src={source}
+            />
+        </picture>
+    )
+}
+export default PictureContain
