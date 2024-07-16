a0fa4b0ae1bb482dca31b58a1f901e95bef5fc16
Shiva pandey <shivapandey9898@gmail.com>
2024-07-16 16:32:34 +0530

    refactor: cleaned primary navbar with picture atom added and map formated

--- src/components/common/header/primary/primary-navbar.jsx
+++ src/components/common/header/primary/primary-navbar.jsx
@@ -1,38 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 // IMPORT REACT COMPONENTS
-import NavigationCard from "@/components/common/header/primary/navigation-cards/NavigationCard"
-import NavigationImgCard from '@/components/common/header/primary/navigation-card-with-image/NavigationImgCard'
+import NavigationCard from "@/components/common/header/primary/navigation-cards/navigation-card"
+import NavigationImgCard from '@/components/common/header/primary/navigation-card-with-image/navigation-image-card'
+
+// IMPORT REACT ATOMS
+import PictureContain from '@/atoms/picture/internal/contain/picture-internal-contain'
 
 const PrimaryNavbar = () => {
     // KEEP TRACK OF NAVLINKS HOVERED STATE
-    const [hoveredLink, setHoveredLink] = useState(null);
+    const [hoveredLink, setHoveredLink] = useState(null)
 
     // KEEP TRACK OF SUBLINK CONTENT HOVERED SATE
-    const [isContentHovered, setIsContentHovered] = useState(false);
+    const [isContentHovered, setIsContentHovered] = useState(false)
 
     // MOUSE LOCATION IN NAVBAR
     const handleMouseEnterNav = (link) => {
-        setHoveredLink(link);
-    };
+        setHoveredLink(link)
+    }
 
     // MOUSE LOCATION OUTSIDE NAVBAR BUT IN SUBLINK CONTENT
     const handleMouseLeaveNav = () => {
         if (!isContentHovered) {
-        setHoveredLink(null);
+        setHoveredLink(null)
         }
-    };
+    }
 
     // MOUSE LOCATION IN SUBLINK CONTENT
     const handleMouseEnterContent = () => {
-        setIsContentHovered(true);
-    };
+        setIsContentHovered(true)
+    }
 
     // MOUSE LOCATION OUTSIDE SUBLINK CONTENT
     const handleMouseLeaveContent = () => {
-        setIsContentHovered(false);
-        setHoveredLink(null);
-    };
+        setIsContentHovered(false)
+        setHoveredLink(null)
+    }
 
     // HARDCODED DATA
     const services_data = [
@@ -57,68 +60,67 @@ const PrimaryNavbar = () => {
         {
             description: "Read the unique stories of farmers who have partnered with us.",
             link:"",
-            source: "/icons/icon-speech.svg",
+            source: "/icons/speech.svg",
             title: "Success Stories",
         },
         {
             description: "Learn about tapping into AgAmerica's nationwide ag finance platform.",
             link:"",
-            source: "/icons/icon-arrows.svg",
+            source: "/icons/arrows.svg",
             title: "Partnerships",
         },
         {
             description: "Stay informed on industry news with insights from our team of experts.",
             link:"",
-            source: "/icons/icon-news.svg",
+            source: "/icons/news.svg",
             title: "Newsroom",
         },
         {
             description: "Join us in our mission to support the American farmer and the agriculture industry.",
             link:"",
-            source: "/icons/icon-building.svg",
+            source: "/icons/building.svg",
             title: "Careers",
         },
-    ];
+    ]
     const insights_data = [
         {
             description: "The latest news and trends impacting the agriculture industry.",
             link: "",
-            source: "/icons/icon-file.svg",
+            source: "/icons/file.svg",
             title: "Blog Articles",
         },
         {
             description: "Our experts dive deeper into economic, legislation, policies, and trends.",
             link: "",
-            source: "/icons/icon-stats-doc.svg",
+            source: "/icons/stats-doc.svg",
             title: "Whitepapers",
         },
         {
             description: "Insights from AgAmerica’s Chief Economist.",
             link: "",
-            source: "/icons/icon-chart.svg",
+            source: "/icons/chart.svg",
             title: "Agricultural Economics",
         },
         {
             description: "Stay informed on the latest policy news.",
             link: "",
-            source: "/icons/icon-bank.svg",
+            source: "/icons/bank.svg",
             title: "Legislation & Trade Updates",
         },
-    ];
+    ]
 
     // LOGO SOURCE FOR DIFFERENT COLOR
-    const logoSrc = hoveredLink ? '/images/AgAmerica-logo-color.svg' : '/images/AgAmerica-logo.svg';
+    const logoSrc = hoveredLink ? '/logo/agamerica-logo-color.svg' : '/logo/agamerica-logo.svg'
 
     return (
         <>
             <section className={`absolute w-full hidden lg:block z-10 ${hoveredLink ? 'bg-white' : ''}`} onMouseLeave={ handleMouseLeaveNav }>
                 <div className="max-w-7xl mx-auto px-4 xl:px-0">
                     <nav className={`flex justify-between items-center bg-transparent relative z-10 py-12 ${hoveredLink ? 'shadow-bottom-only' : ''}`}>
-                        <div className="logo">
-                            <img
-                                alt="logo"
-                                className="object-cover h-7"
-                                src={ logoSrc }
+                        <div className="logo h-7">
+                            <PictureContain
+                                alternative_text="logo"
+                                source={ logoSrc }
                             />
                         </div>
                         <div className="nav-links flex items-center gap-8">
@@ -159,48 +161,45 @@ const PrimaryNavbar = () => {
                                         <div className="flex flex-col gap-2 pr-32 border-r border-brown">   
                                             <div className="py-2 border-b border-brown flex justify-between items-center">
                                                 <a href=""><span className="text-2xl font-bold">Farm Land Fianancing</span></a>
-                                                <img
-                                                    className="h-3"
-                                                    src="/icons/right-arrow-no-line.svg"
-                                                />
+                                                <div className="h-3">
+                                                    <PictureContain
+                                                        alternative_text="right-arrow"
+                                                        source="/icons/right-arrow-no-line.svg"
+                                                    />
+                                                </div>
                                             </div>
                                             <div className="py-2 border-b border-brown">
                                                 <a href=""><span className="text-2xl font-bold text-gray-400 hover:text-black duration-200">Growth Equity</span></a>
                                             </div>
                                             <div className="py-2 flex justify-between items-center">
                                                 <a href=""><span className="text-2xl font-bold text-gray-400 hover:text-black duration-200">Farm Advisory Services</span></a>
-                                                <img
-                                                    className="h-3"
-                                                    src="/icons/right-arrow-no-line.svg"
-                                                />
+                                                <div className="h-3">
+                                                    <PictureContain
+                                                        alternative_text="right-arrow"
+                                                        source="/icons/right-arrow-no-line.svg"
+                                                    />
+                                                </div>
                                             </div>
                                         </div>
                                         <div className="col-span-2 grid grid-cols-2 pl-16">
                                             {/* SERVICES LINKS MAPPED */}
                                             <div className="grid grid-cols-2 items-center gap-4 h-24">
-                                                {services_data.map((service) => (
-                                                    <a href={ service.link }>
-                                                        <span className="text-lg font-bold hover:border-b-2 border-brown py-1">{service.title}</span>
-                                                    </a>
-                                                ))}
+                                                {
+                                                    services_data.map((service) => (
+
+                                                        <a href={ service.link }>
+                                                            <span className="text-lg font-bold hover:border-b-2 border-brown py-1">{ service.title }</span>
+                                                        </a>
+
+                                                    ))
+                                                }
                                             </div>
-                                            <a href="">
-                                                <div className="bg-bg-horse bg-no-repeat bg-cover rounded-xl p-6 flex flex-col justify-between bg-slate-800 hover:bg-none h-full group">
-                                                    <span className="text-white font-semibold">FAST TRACK APPLICATION</span>
-                                                    <a href="" className="relative mt-2">
-                                                        <div className="">
-                                                            <div className="flex items-center gap-1">
-                                                                <span className="text-white text-xs font-medium">APPLY</span>
-                                                                <img
-                                                                    className="w-4 h-4 opacity-0 transition-opacity duration-300 group-hover:opacity-100"
-                                                                    src="/icons/right-arrow.svg"
-                                                                />
-                                                            </div>
-                                                            <div className="absolute -bottom-1 left-0 w-0 h-[1px] bg-white transition-all duration-300 group-hover:w-14"></div>
-                                                        </div>
-                                                    </a>
-                                                </div>
-                                            </a>
+                                            <NavigationImgCard
+                                                bgImage="bg-bg-horse"
+                                                buttonText="APPLY" 
+                                                link=""
+                                                title="FAST TRACK APPLICATION" 
+                                            />
                                         </div>
                                     </div>
                                 }
@@ -216,15 +215,18 @@ const PrimaryNavbar = () => {
                                                 title="MEET OUR TEAM" 
                                             />
                                             <div className="grid grid-cols-4 gap-2">
-                                                {who_we_are_data.map((data) => (
-                                                    <a href={ data.link }>
+                                                {
+                                                    who_we_are_data.map((data) => (
+
                                                         <NavigationCard
-                                                            title={ data.title }
                                                             description={ data.description }
+                                                            link={ data.link }
                                                             source={ data.source }
+                                                            title={ data.title }
                                                         />
-                                                    </a>
-                                                ))}
+
+                                                    ))
+                                                }
                                             </div>
                                         </div>
                                     </div>
@@ -235,15 +237,20 @@ const PrimaryNavbar = () => {
                                     <div className="h-44">
                                         <div className="flex gap-2 h-full">
                                             <div className="grid grid-cols-4 gap-2">
-                                                {insights_data.map((data) => (
-                                                    <a href={ data.link }>
+                                                {
+
+                                                    insights_data.map((data) => (
+
                                                         <NavigationCard
-                                                            title={ data.title }
                                                             description={ data.description }
+                                                            link={ data.link }
                                                             source={ data.source }
+                                                            title={ data.title }
                                                         />
-                                                    </a>
-                                                ))}
+
+                                                    ))
+                                                    
+                                                }
                                             </div>
                                             <NavigationImgCard
                                                 bgImage="bg-bg-vineyard"
@@ -260,7 +267,6 @@ const PrimaryNavbar = () => {
                 </div>
             </section>
         </>
-    );
-};
-
-export default PrimaryNavbar;
+    )
+}
+export default PrimaryNavbar
